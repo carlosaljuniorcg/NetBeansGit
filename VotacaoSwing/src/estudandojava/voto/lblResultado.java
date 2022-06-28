4d0a46638943243981261ebbc36094b9b4be0f1e
Carlos Antônio <carlosaljuniorcg@hotmail.com>
2022-06-28 15:34:18 -0300

    Update 28062022

---
+++ VotacaoSwing/src/estudandojava/voto/lblResultado.java
@@ -0,0 +1,13 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+package estudandojava.voto;
+
+/**
+ *
+ * @author Carlos
+ */
+class lblResultado {
+    
+}
